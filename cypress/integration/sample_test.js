0ae05543acc65256ab44175cf633d460d845761f
Shayan Yazdanmehr <shayan.yazdanmehr@nav.no>
2020-05-15 08:13:27 +0200

    Example tests

--- cypress/integration/sample_test.js
+++ cypress/integration/sample_test.js
@@ -1,10 +1,16 @@
-describe('My First Test', ()=>{
-  it('Does not do much',()=>{
+describe('Sidebar links', ()=>{
+  it('Behandlinger link should redirect us to process page',()=>{
     cy.visit('http://localhost:3000/')
     cy.get('[href="/process"] > .b7 > .b0').click()
     cy.url().should('include','/process')
+
+  })
+})
+
+describe('Test main search', ()=>{
+  it('Number of test results should be more than 0',()=>{
     cy.get('.cb > .d7')
       .type('test')
-
+    cy.get('#bui-1').contains('Alderspensjon').click()
   })
 })
